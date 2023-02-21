99bafe6d4ecb4abd810750a533363b31021fecd4
minhyeok <minhyeok.dev@gmail.com>
2023-02-21 23:19:16 +0900

    mission2 요구 사항 2 적용

--- src/day06/mission2/Main.java
+++ src/day06/mission2/Main.java
@@ -3,12 +3,24 @@ package day06.mission2;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("휴대폰 번호를 입력해주세요. ");
-        String input = scanner.nextLine();
-        PhoneNumber phoneNumber = new PhoneNumber(input);
-        System.out.println("휴대폰 번호를 정상적으로 입력하셨습니다. 입력하신 휴대폰 번호는 " + phoneNumber.getPhoneNumberFormally() + "입니다.");
+        boolean isContinued = true;
+        while (isContinued) {
+            System.out.println("휴대폰 번호를 입력해주세요. ");
+            String input = scanner.nextLine();
+            try {
+                PhoneNumber phoneNumber = new PhoneNumber(input);
+                System.out.println(
+                    "휴대폰 번호를 정상적으로 입력하셨습니다. 입력하신 휴대폰 번호는 " + phoneNumber.getPhoneNumberFormally()
+                        + "입니다.");
+                isContinued = false;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
     }
 }
--- src/day06/mission2/PhoneNumber.java
+++ src/day06/mission2/PhoneNumber.java
@@ -10,11 +10,10 @@ public class PhoneNumber {
     }
 
     private void validate(String phoneNumber) {
-        String input1 = "^01(?:0)(\\d{3}|\\d{4})(\\d{4})$";
-        String input2 = "^\\d{3}\\d{4}\\d{4}$";
-
-        if(!phoneNumber.matches(input1)) { // 010  시작 x 정규식 이용?
-            if(!phoneNumber.matches(input2)){ //11글자 x
+        String input010 = "^01(?:0)(\\d{3}|\\d{4})(\\d{4})$"; // 010 시작하는지 확인 정규식
+        String input11 = "^\\d{3}\\d{4}\\d{4}$"; // 11글자 확인 정규식
+        if (!phoneNumber.matches(input010)) { // 010으로 시작하지 않을 경우
+            if (!phoneNumber.matches(input11)) { //11글자 아닐경우
                 throw new IllegalArgumentException("휴대폰 번호는 11글자여야 합니다.");
             }
             throw new IllegalArgumentException("휴대폰 번호는 010으로 시작해야 합니다.");
@@ -22,7 +21,7 @@ public class PhoneNumber {
     }
 
     public String getPhoneNumberFormally() {
-        String regEx = "(\\d{3})(\\d{3,4})(\\d{4})";
-        return this.value.replaceAll(regEx,"$1-$2-$3");
+        String regEx = "(\\d{3})(\\d{3,4})(\\d{4})"; // 휴대폰 번호 포맷 정규식
+        return this.value.replaceAll(regEx, "$1-$2-$3");
     }
 }
