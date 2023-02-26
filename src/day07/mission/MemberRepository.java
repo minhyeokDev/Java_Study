53593b834a6d305edf6ccb8c36f6142b2b2354f4
minhyeok <minhyeok.dev@gmail.com>
2023-02-26 15:28:47 +0900

    mission 2단계 구현2

--- src/day07/mission/MemberRepository.java
+++ src/day07/mission/MemberRepository.java
@@ -4,20 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class MemberRepository extends Member{
+public class MemberRepository extends Member {
 
     private List<Member> members = new ArrayList<>();
 
-    public void add(String email, String name , int age) {
+    public void add(String email, String name, int age) {
 
         if (email.equals(this.getEmail())) {
             throw new IllegalArgumentException("이미 등록된 이메일이어서 회원 등록에 실패했습니다.");
-        }
-        else{
+        } else {
             this.setEmail(email);
             this.setName(name);
             this.setAge(age);
         }
+    }
 
+    public void check(String name) {
+        if (!name.equals(this.getName())) {
+            throw new IllegalArgumentException("등록되지 않은 회원입니다.");
+        } else if (name.equals(this.getName())) {
+            System.out.println(
+                this.getName() + "님의 이메일은 " + this.getEmail() + "이고, 나이는 " + this.getAge()
+                    + "살입니다.");
+        }
     }
 }
